8a321fa43a74a682fcfbafba0f678ee3053d790d
Anoop Jayaram <66719752+anoopjayaram@users.noreply.github.com>
2024-12-13 15:02:05 +0530

    Update main.c

--- PWM/main.c
+++ PWM/main.c
@@ -1,27 +1,20 @@
 #include<lpc213x.h>
 
-void main() {
-
-PINSEL0=1<<3;
-
-IO0DIR=~0;
-
-PWMMR0=10; //COMPARED TO TIMER / C VALUE - PERIOD 10 * 10mS = 100ms
-
-PWMMR3=5; // 50% Duty Cycle 50ms ON Time
-
-PWMTCR=2;
-
-PWMTCR=1|1<<3;
-
-PWMMCR=1<<1;
-
-PWMPR=149999; //1 mS
-
-PWMTC=0;
-
-PWMPC=0; 
-  PWMPCR=1<<11;
-while(1) {
-
-} }
+void main()
+{
+  PINSEL0=1<<3;
+  IO0DIR=~0;
+  PWMMR0=10; //Pwm Timer  Period = 100ms ( 10 * 10mS)
+  PWMMR3=5; // 50% Duty Cycle 50ms ON Time
+  PWMTCR=2; // Counter ReseT
+  PWMTCR=1|1<<3; // Counter Enable | PWM Enable
+  PWMMCR=1<<1; // Reset on PWMMR0: the PWMTC will be reset if PWMMR0 matches it.
+  PWMPR=149999; //1 mS Timer time period
+  PWMTC=0;
+  PWMPC=0; 
+  PWMPCR=1<<11; //The PWM3 output enabled.
+    while(1)
+    {
+    
+    }
+}
